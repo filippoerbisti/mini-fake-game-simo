847d412cbb996e1854b6c9d35be2a0f0ce528b1e
filippoerbisti <filippo.erbisti@gmail.com>
2023-10-21 16:42:46 +0200

    fix

--- main.js
+++ main.js
@@ -10,9 +10,12 @@ function start() {
     const audio = document.querySelector("audio");
     const intro = document.getElementById("intro");
     const game = document.getElementById("story");
+    const body = document.getRootNode().body;
+    console.log(body)
     if(confirm('Ruotare lo schermo pls :)')) {
         audio.pause();
         intro.style.display = 'none';
+        body.style.overflow = 'auto';
         game.style.display = 'flex';
     }
 }
